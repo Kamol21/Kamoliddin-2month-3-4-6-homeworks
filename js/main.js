415d41f26905bf57ba1dd85532b0895c3cdf25ea
Kamol21 <kamolakangdev@gmail.com>
2022-06-28 06:48:35 +0500

    fix all problems and add about us page

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var elModalOpenButton = document.querySelector(".s1-button");
+var elModal = document.querySelector(".modal");
+
+elModalOpenButton.addEventListener("click", function(){
+  elModal.classList.add("modal-show");
+})
+
+
+var elModalCloseButton = document.querySelector(".modal-close-button");
+
+elModalCloseButton.addEventListener("click" , function (){
+  elModal.classList.remove("modal-show");
+})
